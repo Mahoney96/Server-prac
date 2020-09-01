02ddb07ee142d2f89f34c9bdc305fce67ae4dd7b
Mahoney96 <cmahoney966@gmail.com>
2020-09-01 13:15:52 -0700

    controller/server Notes Added

--- server/controller.js
+++ server/controller.js
@@ -1,8 +1,66 @@
 const shoes = require("../shoes-db.json")  
-
+let id = shoes[shoes.length-1].id+1;
 
 module.exports = {
     getShoes: (req, res) => {
-        res.status(200).send(shoes)  
-    }
+        console.log(shoes)
+        res.status(200).send(shoes)
+        
+    },
+    addShoe: (req, res) =>  {
+        const{name, img, inventory, description, price} = req.body
+        const newShoe = {
+            id, name, img, inventory, description, price
+        }
+        id++
+        shoes.push(newShoe)
+        res.status(200).send(shoes)
+        },
+    addShoeAgain: (req, res) => {},
+
+    getOneShoe: (req, res) => {},
+
+    getSavedShoes: (req, res) => {},
+
 };
+
+//  (addShoe) Step-Breakdown:
+
+// 1) Declare the new function and its params: addShoe: (req, res)
+// 2) invoke function using arrow function:  =>  // Note: cannot be used as constructors.
+// 3) Make object for variable func: write the function/functions were using to make the changes wanted:
+
+
+// 1) How are we altering the Data
+// 2) Where is the data we're handling (the data we're about to do something with)
+// 3) What is the function(s) we're using to make those changes to the data
+
+            // // Ex. - functionality that addShoe is adding with the post request:
+
+                // {const{name, img, inventory, description, price} = req.body
+                // const newShoe = {
+                //     id, name, img, inventory, description, price
+                // }
+                // id++
+                // shoes.push(newShoe)
+                // res.status(200).send(shoes)
+                // },
+
+            // NOTE:    How the above object function is written in the controller file for this object function:
+                        // app.post('/api/shoes', ctrl.addShoe) 
+
+            //WHY:      Why do I need it in both the controller and index file?
+                //      - Notice the difference in how they're being written;
+                //      - Does one have more functionality than the other?
+
+            // **YES** :  For server/index.js - app.get('api/shoes', ctrl/getShoes)
+                                        // This is a reference:
+                                        // 1) To the index URL
+                                        // 2) The ctrl(controller) function in controller.js
+
+
+
+
+
+
+
--- server/index.js
+++ server/index.js
@@ -1,31 +1,48 @@
 const express = require('express');
-const controller = require('./controller');
+
 // massive = require('massive');
 const ctrl = require('./controller.js');
 // console.log(shoes)
 
-const app = express();
+const app = express();  
 
-app.use(express.json());  //json is the lnaguage that lets browsers communicate with any server. Its going to send the info back in JSON.
+app.use(express.json());  //json is the lAnaguage that lets browsers communicate with server. Its going to send the info back in JSON.
 
 const SERVER_PORT = 3993;
 
-//
+
 app.get('/api/shoes',ctrl.getShoes) 
+
+app.post('/api/shoes', ctrl.addShoe)
+
+
+
+
+
+
 
 app.listen(SERVER_PORT, () => console.log(`You're servers Ruunnin on ${SERVER_PORT} Aright Aright Aright`));
 
-// app = require(express.json());
+// app.post('/api/shoes')  // -- What are the 2 parts to this request?
+                        // 1) the URL path/address: where retreiving the request from
+                        // 2) The handler: the action at the URL 
 
 //endpoint URL, this is an endpoint. Every endpoint has two parts , the url and the handler
-// URL - like the label of the itme on the menu
+// URL - like the label of the item on the menu
 // handler - do the logic
-//  send() - what were actually sending the the ffornt end
+//  send() - what were actually sending the the fornnt end
 
 // we calll it the End
-// BEcause our server knows hows to handle the 
+// Because our server knows hows to handle the 
 
 // the URL we're sending in POSTMAN has a domain 
 // We call it the endpoint, because it's at the END of our URL
-// Our server is listening 
+// Our server is listening  to 
 // based on what endpoints
+
+// two parts 1) URL  2) THe handler - Always a function and it handles the request. BEcause were listening on port 3993 were waiting to hear if any requests come in with the endpoint /api /api/shoes. IF we get the request the handler will send 
+//- Chefs knos how ot make a grill cheese sanwhich
+// if chef knows how to make  a sandwich but is all out of cheese then - sending the 
+// if we sent a requests to the chef for something 
+// .catch - I know how to make a grille dcheese sanwich but we're all out of cheese
+// .sandals wouldn't work because we are trying to send a request for something we don't have (like something the chef doesn't know how to make)
